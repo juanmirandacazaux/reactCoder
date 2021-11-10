b2d908af74ad1f8afcedc18f4b6ef5c5f15d2a81
Juan Miranda <juanmirandacazaux@gmail.com>
2021-11-10 19:38:25 -0300

    Desafio 4

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+const NavBar = () => {
+
+    return ( 
+    
+    <div className="header">
+        <nav>
+           <ul>
+               
+           <li>Hola</li>
+           <li>Hola</li>
+           <li>Hola</li>
+           </ul>
+        </nav>
+    </div>)
+}
+
+export default NavBar
